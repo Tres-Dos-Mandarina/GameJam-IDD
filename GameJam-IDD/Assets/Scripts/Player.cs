30259807852260a64c05c28d6c3ba91330204ffc
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-09-19 11:13:32 +0200

    miniCommit

--- GameJam-IDD/Assets/Scripts/Player.cs
+++ GameJam-IDD/Assets/Scripts/Player.cs
@@ -87,13 +87,13 @@ public class Player : MonoBehaviour
             jumpCount--;
         }
 
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0 && jumpCount > 0)
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
         {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * jumpDamping);
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
             jumpCount--;
         }
 
-        isJumping = false;
+        isJumping = false; // ?
     }
 
     private void Animate()
