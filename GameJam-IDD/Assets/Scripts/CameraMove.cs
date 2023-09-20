644be95b3a39c4657e4ac5f2c581d5af92bd1627
MHF13 <41444696+MHF13@users.noreply.github.com>
2023-09-20 18:27:39 +0200

    CameraMoveScript

---
+++ GameJam-IDD/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+
+    public Rigidbody2D playerRB = null;
+
+    /*
+    public Player player = null;
+    public Vector3 offset = new Vector3(0, 0, -5);
+    [Range(0f, 1f)]
+    public float smooth = 0.01f;
+    public float smoothLock = 4;
+    */
+    private Vector3 velocity;
+    //---
+    private float newPos = 0;
+    public float cameraSpeed = 0;
+    public float distance = 0;
+
+    private int direction = 0;
+    private float lastspeed = 0;
+
+    [Range(-3f, 3f)]
+    public float verticalPos = 0;
+
+    private void LateUpdate()
+    {
+        Move5();
+    }
+
+    /*public void Move()
+    {
+        Vector3 target = player.gameObject.transform.position + offset;
+        Vector3 movement = Vector3.Lerp(transform.position, target, smooth);
+
+        transform.position = movement;
+    }
+    public void Move2()
+    {
+        velocity = playerRB.velocity;
+        Vector3 target = playerRB.position;
+        Vector3 movement = Vector3.zero;
+
+        //movement = Vector3.Lerp(transform.position, new Vector3(target.x, target.y, transform.position.z) + velocity, newSpeed);
+
+        //if (velocity.x < player.moveSpeed - 1 || velocity.x > -player.moveSpeed + 1) 
+        if (velocity.x < player.moveSpeed - smoothLock && velocity.x > -player.moveSpeed + smoothLock)
+        {
+            Debug.Log("AA");
+            movement = Vector3.Lerp(transform.position, new Vector3(target.x, target.y, transform.position.z) + velocity, smooth);
+        }
+        else
+        {
+            Debug.Log("BB");
+
+            movement = new Vector3(target.x + (playerRB.velocity.x / 100f * 90f), target.y, transform.position.z);
+        }
+        transform.position = movement;
+    }
+
+    public void Move3()
+    {
+        velocity = playerRB.velocity;
+        Vector3 target = playerRB.position;
+
+        if (velocity.x < player.moveSpeed - smoothLock && velocity.x > -player.moveSpeed + smoothLock)
+            transform.position = new Vector3(target.x + (playerRB.velocity.x / 100f * 90f), target.y, transform.position.z);
+        else if (playerRB.velocity.x > smoothLock)
+        {
+            transform.position = new Vector3(target.x + (smoothLock / 100f * 90f), target.y, transform.position.z);
+        }
+        else if (playerRB.velocity.x < -smoothLock)
+        {
+            transform.position = new Vector3(target.x + (-smoothLock / 100f * 90f), target.y, transform.position.z);
+        }
+
+    }
+    public void Move4()
+    {
+        velocity = playerRB.velocity;
+        Vector3 target = playerRB.position;
+
+
+        transform.position = new Vector3(playerRB.position.x + lookAhead, transform.position.y, transform.position.z);
+
+
+
+        if (velocity.x != 0)
+        {
+            lookAhead = Mathf.Lerp(lookAhead, aheadDistance * (velocity.x / Mathf.Abs(velocity.x)), Time.deltaTime * cameraSpeed);
+        }
+        else
+        {
+            lookAhead = Mathf.Lerp(lookAhead, 0, Time.deltaTime * cameraSpeed);
+        }
+    }*/
+
+    public void Move5()
+    {
+        velocity = playerRB.velocity;
+
+        direction = (velocity.x > 0) ? 1 : -1;
+        if (Mathf.Abs(velocity.x) < lastspeed) direction = 0;
+        if (velocity.x == 0) direction = 0;
+
+        transform.position = new Vector3(playerRB.position.x + newPos, playerRB.position.y+verticalPos, transform.position.z);
+        newPos = Mathf.Lerp(newPos, distance * direction, Time.deltaTime * cameraSpeed);
+
+        lastspeed = Mathf.Abs(velocity.x);
+    }
+}
+    
+
+
+
