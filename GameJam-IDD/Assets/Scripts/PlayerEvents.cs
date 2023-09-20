66514fc0ad1492bf6667a82b2badb6ca98236849
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-09-20 18:59:48 +0200

    new camera follow and fixe ddeath

--- GameJam-IDD/Assets/Scripts/PlayerEvents.cs
+++ GameJam-IDD/Assets/Scripts/PlayerEvents.cs
@@ -27,9 +27,7 @@ public class PlayerEvents : MonoBehaviour
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-            Debug.Log("sdasda");
-            _onLand.Raise(this, " ");
+        _onLand.Raise(this, " ");
         
         if (collision.gameObject.CompareTag("Death"))
         {
