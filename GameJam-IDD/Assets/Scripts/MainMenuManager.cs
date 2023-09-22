092338e4886b8c5300b6d90d67e269a706be6c61
Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>
2023-09-22 14:51:43 +0200

    MainMenu Methods created
    Scenes linked

---
+++ GameJam-IDD/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private static MainMenuManager Instance { get; set; }
+    
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Abrir apartado de Settings");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
