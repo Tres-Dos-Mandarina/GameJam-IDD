291892079a706a60be7aa7031dc893ba01416166
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-09-22 17:59:30 +0200

    save data

---
+++ GameJam-IDD/Assets/Scripts/SaveData.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData : MonoBehaviour
+{
+    public GameConfig config;
+    public void SaveToJson(bool _speedrun, bool _audio, bool _kenkri)
+    {
+        config.speedRun = _speedrun;
+        config.audioOn = _audio;
+        config.kenkri = _kenkri;
+        string configSettings = JsonUtility.ToJson(config);
+        string filePath = Application.persistentDataPath + "/oriol_gilipollas.json";
+        Debug.Log(filePath);
+        System.IO.File.WriteAllText(filePath, configSettings);
+    }
+    public void LoadFromJson()
+    {
+        string filePath = Application.persistentDataPath + "/oriol_gilipollas.json";
+        string settings = System.IO.File.ReadAllText(filePath);
+
+        config = JsonUtility.FromJson<GameConfig>(settings);
+    }
+}
+
+[System.Serializable]
+public class GameConfig
+{
+    public bool speedRun = false;
+    public bool audioOn = false;
+    public bool kenkri = false;
+}
