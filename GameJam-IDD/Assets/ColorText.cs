5eb805b5daa71f1fdfe7ea05ce85ea52fad450b8
MHF13 <41444696+MHF13@users.noreply.github.com>
2023-09-24 19:59:55 +0200

    Menu Color Text

--- GameJam-IDD/Assets/ColorText.cs
+++ GameJam-IDD/Assets/ColorText.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ColorText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    private TMP_Text theText;
+
+    public Color32[] colors = { new Color32(127, 127, 127,255), new Color32(209, 209, 209,255) };
+
+    private void Awake()
     {
-        
+        theText = this.transform.GetChild(0).GetComponent<TMP_Text>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerEnter()
     {
-        
+        theText.color = colors[1]; //Or however you do your color
+    }
+
+    public void OnPointerExit()
+    {
+        theText.color = colors[0]; //Or however you do your color
     }
 }
