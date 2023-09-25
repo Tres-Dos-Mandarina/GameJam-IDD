fe207a586556239b2f6272f0b4f8db94aea80d0d
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-09-25 13:57:24 +0200

    modified goal and tested it all levels are good

--- GameJam-IDD/Assets/Scripts/Goal.cs
+++ GameJam-IDD/Assets/Scripts/Goal.cs
@@ -7,26 +7,51 @@ public class Goal : MonoBehaviour
     public GameEvent popUpExit;
     bool isLightOff = false;
     bool isEventCalled = false;
+    public float distanceToPlayer = 3f;
+    private bool sendMessage = false;
+    private Transform player;
+    private void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<Transform>();
+    }
+    private void Update()
+    {
+        if(Vector3.Distance(player.position, transform.position) < distanceToPlayer)
+        {
+            if(!isEventCalled && !sendMessage && !isLightOff)
+            {
+                    isEventCalled = true;
 
+                    popUpEnter.Raise(this, 1);
+                    sendMessage = true;
+                
+            }
+        }
+        else
+        {
+            isEventCalled = false;
+            sendMessage = false;
+            popUpExit.Raise(this, 1);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (!collider.CompareTag("Player")) return;
         if(isLightOff)
         {
-            Debug.Log("Sending Goal Event");
             onGoalReached.Raise(this, null); //GM
         }
-        else if (!isEventCalled)
-        {
-            isEventCalled = true;
-            popUpEnter.Raise(this, 1);
-        }
+        //else if (!isEventCalled)
+        //{
+        //    isEventCalled = true;
+        //    popUpEnter.Raise(this, 1);
+        //}
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (!collision.CompareTag("Player")) return;
-        isEventCalled = false;
-        popUpExit.Raise(this, 1);
+        //isEventCalled = false;
+        //popUpExit.Raise(this, 1);
     }
     public void IsLightOff(Component sender, object data)
     {
