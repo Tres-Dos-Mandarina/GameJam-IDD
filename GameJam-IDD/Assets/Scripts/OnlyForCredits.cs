3ce9b874d6bd3daf9dd1ad66539f6bc20858f8dd
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-09-25 13:24:56 +0200

    credits now can be skipped after 4s and pressing anykey

--- GameJam-IDD/Assets/Scripts/OnlyForCredits.cs
+++ GameJam-IDD/Assets/Scripts/OnlyForCredits.cs
@@ -5,23 +5,34 @@ using UnityEngine.SceneManagement;
 public class OnlyForCredits : MonoBehaviour
 {
     private Animator anim;
+    private bool canType = false;
     private void Awake()
     {
         anim = FindObjectOfType<Animator>();
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
     }
     // Start is called before the first frame update
     void Start()
     {
         anim.Play("Credits");
+        StartCoroutine(WaitToType());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKey && canType)
+            GoToMainMenu();
     }
     public void GoToMainMenu()
     {
         SceneManager.LoadScene("_MainMenu");
     }
+    IEnumerator WaitToType()
+    {
+        yield return new WaitForSeconds(4f);
+        canType = true;
+    }
+
 }
